fc37973f9403b827d8c10b5650a35fe4e6458706
Quenim <KN17022004@yandex.ru>
2025-07-09 03:30:03 +0500

    v0.0.3

--- backend/main.py
+++ backend/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 from backend.bots.manager import init_all_bots, bots
 import asyncio
+import random
 
 class MessageRequest(BaseModel):
     username: str
@@ -11,7 +12,7 @@ app = FastAPI()
 
 @app.on_event("startup")
 async def startup():
-    await init_all_bots(channel="quenimx")  # ← Замени на свой канал
+    await init_all_bots(channel="zhume211")  # ← Замени на свой канал
 
 @app.post("/send")
 async def send_message(data: MessageRequest):
@@ -20,3 +21,18 @@ async def send_message(data: MessageRequest):
         raise HTTPException(status_code=404, detail="Бот не найден")
     await bot.send_message(data.message)
     return {"status": "sent", "from": data.username}
+
+class MessageRequest(BaseModel):
+    message: str
+
+@app.post("/send_all")
+async def send_message_all(req: MessageRequest):
+    if not bots:
+        raise HTTPException(status_code=500, detail="Боты не инициализированы")
+    
+    # каждому боту по очереди отправляем сообщение
+    for bot in bots.values():
+        await bot.send_message(req.message)
+        await asyncio.sleep(random.uniform(2, 4))  # интервал между ботами
+    
+    return {"status": "sent", "bots": list(bots.keys())}
